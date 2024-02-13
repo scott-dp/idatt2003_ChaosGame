9c74edb8d94ac7ab63a172a71fe6284ca0e56d12
Scott <scottld@stud.ntnu.no>
2024-02-13 14:38:13 +0100

    Added javadocs to afinne transform class

--- src/main/java/edu/ntnu/stud/models/AffineTransform2D.java
+++ src/main/java/edu/ntnu/stud/models/AffineTransform2D.java
@@ -1,14 +1,36 @@
 package edu.ntnu.stud.models;
 
+/**
+ * The AffineTransform2D class represents an affine transformation in two-dimensional space.
+ * <p>
+ * This class implements the Transform2D interface.
+ * </p>
+ *
+ * @author Scott du Plessis
+ */
+
 public class AffineTransform2D implements Transform2D {
   private final Matrix2x2 matrix;
   private final Vector2D vector;
 
+  /**
+   * Constructs a new AffineTransform2D object with
+   * the specified transformation matrix and translation vector.
+   *
+   * @param matrix The 2x2 transformation matrix.
+   * @param vector The translation vector.
+   */
   public AffineTransform2D(Matrix2x2 matrix, Vector2D vector) {
     this.matrix = matrix;
     this.vector = vector;
   }
 
+  /**
+   * Transforms the specified 2D vector using this affine transformation.
+   *
+   * @param point The 2D vector to be transformed.
+   * @return The transformed vector.
+   */
   @Override
   public Vector2D transform(Vector2D point) {
     Vector2D result = matrix.multiply(point);
