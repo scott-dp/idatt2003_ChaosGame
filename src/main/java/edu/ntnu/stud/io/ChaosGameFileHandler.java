f695c80e2d0511d12cb098cc3f661e7e2ce35093
Scott <scottld@stud.ntnu.no>
2024-03-04 19:25:58 +0100

    Added comments to methods for implementation

--- src/main/java/edu/ntnu/stud/io/ChaosGameFileHandler.java
+++ src/main/java/edu/ntnu/stud/io/ChaosGameFileHandler.java
@@ -2,13 +2,18 @@ package edu.ntnu.stud.io;
 
 import edu.ntnu.stud.config.ChaosGameDescription;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class ChaosGameFileHandler {
 
   public ChaosGameDescription readFromFile(String path) {
-    // Read from file
+    //use scanner - se oppgavetekst under sierpinski trekant om denne klassen
   }
 
   public void writeToFile(ChaosGameDescription description, String path) {
-    // Write to file
+    // Write to file using BufferedWriter
   }
 }
