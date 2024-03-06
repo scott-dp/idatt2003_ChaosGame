9362ea403cb49417cd6360daa4475943dac4b324
Scott <scottld@stud.ntnu.no>
2024-03-06 16:48:01 +0100

    Tried using delimiter to split file but didnt come very far

--- src/main/java/edu/ntnu/stud/io/ChaosGameFileHandler.java
+++ src/main/java/edu/ntnu/stud/io/ChaosGameFileHandler.java
@@ -2,15 +2,16 @@ package edu.ntnu.stud.io;
 
 import edu.ntnu.stud.config.ChaosGameDescription;
 
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
+import java.util.Scanner;
 
 public class ChaosGameFileHandler {
 
-  public ChaosGameDescription readFromFile(String path) {
-    //use scanner - se oppgavetekst under sierpinski trekant om denne klassen
+  public ChaosGameDescription readFromFile(String path) throws FileNotFoundException {
+    //TODO validate path/file
+    File file = new File(path);
+    Scanner scanner = new Scanner(file);
+    scanner.useDelimiter("#");
   }
 
   public void writeToFile(ChaosGameDescription description, String path) {
