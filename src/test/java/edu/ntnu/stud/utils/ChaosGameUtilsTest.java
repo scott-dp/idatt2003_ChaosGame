56fea3310f0e8e2e53ab83701c242a401f40a5b6
Scott <scottld@stud.ntnu.no>
2024-03-11 17:25:07 +0100

    Added tests for util methods

--- src/test/java/edu/ntnu/stud/utils/ChaosGameUtilsTest.java
+++ src/test/java/edu/ntnu/stud/utils/ChaosGameUtilsTest.java
@@ -1,7 +1,36 @@
 package edu.ntnu.stud.utils;
 
 import org.junit.jupiter.api.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class ChaosGameUtilsTest {
+
+  @Nested
+  class PositiveTests {
+
+    @Test
+    void testAreDoublesEqual() {
+      assertTrue(ChaosGameUtils.areDoublesEqual(1.001, 1.001));
+    }
+
+    @Test
+    void testRoundDoubleToSetDecimals() {
+      assertEquals(ChaosGameUtils.roundDoubleToSetDecimals(2.817382, 3), 2.817);
+      assertEquals(ChaosGameUtils.roundDoubleToSetDecimals(5.8899, 3), 5.890);
+    }
+
+    @Test
+    void testValidatePositiveInteger() {
+      assertEquals(ChaosGameUtils.validatePositiveInteger(1), 1);
+    }
+  }
+
+  @Nested
+  class NegativeTests {
+    @Test
+    void testValidatePositiveInteger() {
+      assertThrows(IllegalArgumentException.class, () -> ChaosGameUtils.validatePositiveInteger(0));
+    }
+  }
 
 }
