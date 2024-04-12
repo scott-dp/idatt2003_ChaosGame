44d86ff18018281f9640165739e1228338572286
Scott <scottld@stud.ntnu.no>
2024-04-12 14:48:34 +0200

    Added main app view and checked to see if it works!

--- src/main/java/edu/ntnu/stud/views/ChaosGameView.java
+++ src/main/java/edu/ntnu/stud/views/ChaosGameView.java
@@ -2,27 +2,37 @@ package edu.ntnu.stud.views;
 
 import edu.ntnu.stud.models.chaosgamehandling.ChaosCanvas;
 import edu.ntnu.stud.models.chaosgamehandling.ChaosGame;
+import edu.ntnu.stud.models.interfaces.ChaosGameObserver;
+import javafx.application.Application;
 import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.layout.Region;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
-public class ChaosGameView {
+public class ChaosGameView implements ChaosGameObserver {
   private final ChaosGame game;
+  VBox mainLayout;
 
-
-  public ChaosGameView(ChaosGame game) {
+  public ChaosGameView(ChaosGame game, VBox mainLayout) {
     this.game = game;
+    this.mainLayout = mainLayout;
   }
 
   public ChaosGame getGame() {
     return game;
   }
 
-  public void showFractal(StackPane layout) {
+  public VBox getMainLayout() {
+    return mainLayout;
+  }
+
+  public Canvas makeFractal() {
+    game.runSteps(10000);
     ChaosCanvas fractalCanvas = game.getCanvas();
     int[][] fractalList = fractalCanvas.getCanvas();
     Canvas canvas = new Canvas(fractalCanvas.getWidth(), fractalCanvas.getHeight());
@@ -38,6 +48,15 @@ public class ChaosGameView {
       }
     }
 
-    layout.getChildren().add(canvas);
+    return canvas;
+  }
+
+  public void drawLayout (Canvas fractal) {
+    mainLayout.getChildren().add(fractal);
+  }
+
+  @Override
+  public void update() {
+    drawLayout(makeFractal());
   }
 }
