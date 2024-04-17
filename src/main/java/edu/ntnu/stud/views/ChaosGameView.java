924564f340ddd14c7ae66e87e918abf4abb2f62b
Scott <scottld@stud.ntnu.no>
2024-04-17 14:09:26 +0200

    Added javadocs to ChaosGAmeView

--- src/main/java/edu/ntnu/stud/views/ChaosGameView.java
+++ src/main/java/edu/ntnu/stud/views/ChaosGameView.java
@@ -7,12 +7,17 @@ import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.paint.Color;
 
 /**
- * View class to show
+ * View class for the ChaosGame class. Implements the {@link ChaosGameObserver} interface.
  */
 public class ChaosGameView implements ChaosGameObserver {
   private final ChaosGame game;
   Canvas canvas;
 
+  /**
+   * Constructs an instance of ChaosGameView with a set ChaosGame.
+   *
+   * @param game the ChaosGame object being shown
+   */
   public ChaosGameView(ChaosGame game) {
     this.game = game;
     this.canvas = new Canvas(game.getChaosCanvas().getWidth(), game.getChaosCanvas().getHeight());
@@ -26,6 +31,9 @@ public class ChaosGameView implements ChaosGameObserver {
     return canvas;
   }
 
+  /**
+   * Adds the fractal to a {@link GraphicsContext} object.
+   */
   public void makeFractal() {
     int[][] fractalList = game.getChaosCanvas().getCanvas();
 
@@ -44,6 +52,9 @@ public class ChaosGameView implements ChaosGameObserver {
 
   }
 
+  /**
+   * Clears the {@link GraphicsContext} object.
+   */
   public void clearGraphicsContext() {
     int[][] fractalList = game.getChaosCanvas().getCanvas();
     GraphicsContext gc = canvas.getGraphicsContext2D();
@@ -55,6 +66,10 @@ public class ChaosGameView implements ChaosGameObserver {
     }
   }
 
+  /**
+   * Updates the fractal. Is called when a change has been made in the ChaosGame
+   * object that this object is observing.
+   */
   @Override
   public void update() {
     makeFractal();
