cdff8c9b7d337400a12dcd58e8fe52a485bff9b3
nislo7 <st.mockus@gmail.com>
2024-04-25 18:49:35 +0200

    created a abstract class for AddJuliaTransformView

--- src/main/java/edu/ntnu/stud/views/AddJuliaTransformView.java
+++ src/main/java/edu/ntnu/stud/views/AddJuliaTransformView.java
@@ -7,6 +7,7 @@ import edu.ntnu.stud.models.Transform2D;
 import edu.ntnu.stud.models.Vector2D;
 import edu.ntnu.stud.models.chaosgamehandling.ChaosGameDescription;
 import edu.ntnu.stud.models.utils.ChaosGameUtils;
+import javafx.event.ActionEvent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
@@ -32,41 +33,17 @@ import java.util.ArrayList;
  * @see ChaosGameDescription
  * @see ChaosGameController
  */
-public class AddJuliaTransformView {
-    private final ChaosGameController chaosGameController = AppView.getChaosGameController();
+public class AddJuliaTransformView extends AbstractJuliaTransformView {
 
-    TextField imaginaryPartField;
-    TextField realPartField;
-
-    //Elements minCoords
-    private TextField minX0;
-    private TextField minX1;
-
-    //Elements maxCoords
-    private TextField maxX0;
-    private TextField maxX1;
-
-    private final Stage stage = new Stage();
-    private final Scene scene = new Scene(new VBox());
-
-    /**
-     * Method that shows the stage.
-     * The method calls {@link #setScene()} to set the scene of the stage.
-     * The method then sets the scene of the stage and shows the stage.
-     */
-    public void showStage() {
-        setScene();
-        stage.setScene(scene);
-        stage.initModality(Modality.APPLICATION_MODAL);
-        stage.show();
+    public AddJuliaTransformView() {
+        super();
     }
 
-    /**
-     * Method that sets the scene of the stage.
-     */
+    @Override
     public void setScene() {
-        this.scene.setRoot(setMainLayout());
+        scene.setRoot(setMainLayout());
     }
+
 
     /**
      * Method that sets the main layout of the Interface.
@@ -74,79 +51,24 @@ public class AddJuliaTransformView {
      *
      * @return mainLayout the main layout container of the interface
      */
+    @Override
     public VBox setMainLayout() {
-        VBox mainLayout = new VBox();
+        VBox mainLayout = new VBox(10);
 
-        mainLayout.getChildren().addAll(topRowContainer(), bottomRowContainer(), saveButton());
+        mainLayout.getChildren().addAll(createNumeralInput(),getCoordinatesInput(),createSaveButton());
 
         return mainLayout;
     }
 
-    /**
-     * Method that creates the top row container.
-     * The top container consists of a label and a text field for the real part of the complex number.
-     * There is an extra vertical container that contains two text fields for the min coordinates.
-     * This container is then added to the horizontal container.
-     * @return topRowContainer the container for the real part of the complex number and the min coordinates
-     */
-    public HBox topRowContainer() {
-        HBox topRowContainer = new HBox(10);
-
-        Label realPartLabel = new Label("Real part:");
-        realPartField = new TextField("Enter Real Part");
-
-        Label minCoordsLabel = new Label("Min Coords:");
-
-        VBox minCoordsContainer = new VBox(10);
-        minX0 = new TextField("0");
-        minX1 = new TextField("0");
-        minCoordsContainer.getChildren().addAll(minX0, minX1);
-
-        topRowContainer.getChildren().addAll(realPartLabel, realPartField, minCoordsLabel, minCoordsContainer);
-
-        return topRowContainer;
-    }
-
-    /**
-     * Method that creates the bottom row container.
-     * The bottom container consists of a label and a text field for the imaginary part of the complex number.
-     * There is an extra vertical container that contains two text fields for the max coordinates.
-     * This container is then added to the horizontal container.
-     * @return bottomRowContainer the container for the imaginary part of the complex number and the max coordinates
-     */
-    public HBox bottomRowContainer() {
-        HBox bottomRowContainer = new HBox(10);
-
-        Label imaginaryPartLabel = new Label("Imaginary part:");
-        imaginaryPartField = new TextField("Enter Imaginary Part");
-
-        Label maxCoordsLabel = new Label("Max Coords:");
-
-        VBox maxCoordsContainer = new VBox(10);
-        maxX0 = new TextField("0");
-        maxX1 = new TextField("0");
-        maxCoordsContainer.getChildren().addAll(maxX0, maxX1);
-
-        bottomRowContainer.getChildren().addAll(imaginaryPartLabel, imaginaryPartField, maxCoordsLabel, maxCoordsContainer);
-
-        return bottomRowContainer;
-    }
-
-    /**
-     *
-     * @return
-     */
-    public Button saveButton() {
-        Button saveButton = new Button("Save");
-        saveButton.setOnAction(e -> {
-            if (!isInputValid()) {
-                ChaosGameUtils.showErrorAlert("Input is invalid");
-                return;
-            }
+    @Override
+    public void saveButtonAction(ActionEvent event) {
+        if (!isInputValid()) {
+            ChaosGameUtils.showErrorAlert("Input is invalid");
+            return;
+        } else {
             chaosGameController.setChaosGame(createJuliaDescription());
             stage.close();
-        });
-        return saveButton;
+        }
     }
 
     public ChaosGameDescription createJuliaDescription() {
@@ -166,24 +88,5 @@ public class AddJuliaTransformView {
 
     public Vector2D createMaxCoordinates() {
         return new Vector2D(Double.parseDouble(maxX0.getText()), Double.parseDouble(maxX1.getText()));
-    }
-
-    public boolean isInputValid() {
-        try {
-            Double.parseDouble(realPartField.getText());
-            Double.parseDouble(imaginaryPartField.getText());
-            double minX0parsed = Double.parseDouble(minX0.getText());
-            double minX1parsed = Double.parseDouble(minX1.getText());
-            double maxX0parsed = Double.parseDouble(maxX0.getText());
-            double maxX1parsed = Double.parseDouble(maxX1.getText());
-            ChaosGameUtils.validateMinAndMaxCoords(
-                    new Vector2D(minX0parsed, minX1parsed), new Vector2D(maxX0parsed, maxX1parsed));
-            return true;
-        } catch (NumberFormatException e) {
-            return false;
-        } catch (IllegalArgumentException e) {
-            ChaosGameUtils.showErrorAlert(e.getMessage());
-            return false;
-        }
     }
 }
