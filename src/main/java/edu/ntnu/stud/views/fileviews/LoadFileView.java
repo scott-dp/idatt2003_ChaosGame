302f82c6bf73501e0d6bead8ea41da74064b8171
Scott <scottld@stud.ntnu.no>
2024-05-20 13:20:40 +0200

    Javadoc and checkstyled savefileviews

--- src/main/java/edu/ntnu/stud/views/fileviews/LoadFileView.java
+++ src/main/java/edu/ntnu/stud/views/fileviews/LoadFileView.java
@@ -3,10 +3,16 @@ package edu.ntnu.stud.views.fileviews;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
+/**
+ * A view that allows the user to load a fractal from a file.
+ */
 public class LoadFileView {
   private final FileChooser fileChooser;
   private final Stage stage;
 
+  /**
+   * Constructs a LoadFileView with a FileChooser and a Stage.
+   */
   public LoadFileView() {
     fileChooser = new FileChooser();
     fileChooser.getExtensionFilters().add(
@@ -15,6 +21,11 @@ public class LoadFileView {
     stage = new Stage();
   }
 
+  /**
+   * Shows the file chooser dialog and returns the chosen file path.
+   *
+   * @return The chosen file path.
+   */
   public String getChosenFilePath() {
     return fileChooser.showOpenDialog(stage).toString();
   }
